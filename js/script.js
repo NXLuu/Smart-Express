753e5e410bd9f27fe8a35313e9012a3b8802e452
Luu Nguyen <nxuluu@gmail.com>
2021-10-30 07:35:06 +0700

    Ui Complete

---
+++ js/script.js
@@ -0,0 +1,13 @@
+﻿let pricingTable = document.querySelector(".pricingtable");
+pricingTable.onmouseover = function(event) {
+    let target = event.target.closest(".pricingtable-wrapper");
+    if (!target) return;
+
+    let isActive = target.classList.contains("active");
+    if (isActive) return;
+
+    let activeTable = pricingTable.querySelector('.active');
+    activeTable.classList.remove("active");
+    target.classList.add("active");
+
+}
